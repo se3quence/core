f85eec82b2aad3cc11294f48a81c97b47d535ff0
thev1ndu <itsthw9@gmail.com>
2025-12-25 14:21:02 +0530

    feat: login & signup page

--- app/(login)/login.tsx
+++ app/(login)/login.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { useActionState } from "react";
 import { useSearchParams } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { CircleIcon, Loader2 } from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
+import { CircleIcon, Loader2, Plus } from "lucide-react";
 import { signIn, signUp } from "./actions";
 import { ActionState } from "@/lib/auth/middleware";
+import { cn } from "@/lib/utils";
 
 export function Login({ mode = "signin" }: { mode?: "signin" | "signup" }) {
   const searchParams = useSearchParams();
@@ -21,135 +24,223 @@ export function Login({ mode = "signin" }: { mode?: "signin" | "signup" }) {
   );
 
   return (
-    <div className="min-h-[100dvh] flex flex-col justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gray-50">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="flex justify-center">
-          <CircleIcon className="h-12 w-12 text-orange-500" />
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        {/* 1. Wrapper for the Card & Decoration Icons only */}
+        <div className="relative mb-6">
+          {/* Decoration Icons */}
+          <Plus
+            className="-top-[12.5px] -left-[12.5px] absolute h-6 w-6 text-muted-foreground/40 z-20"
+            strokeWidth={1}
+          />
+          <Plus
+            className="-top-[12.5px] -right-[12.5px] absolute h-6 w-6 text-muted-foreground/40 z-20"
+            strokeWidth={1}
+          />
+          <Plus
+            className="-bottom-[12.5px] -left-[12.5px] absolute h-6 w-6 text-muted-foreground/40 z-20"
+            strokeWidth={1}
+          />
+          <Plus
+            className="-right-[12.5px] -bottom-[12.5px] absolute h-6 w-6 text-muted-foreground/40 z-20"
+            strokeWidth={1}
+          />
+
+          {/* The Main Box */}
+          <Card className="overflow-hidden border border-border rounded-none shadow-none relative z-10">
+            <CardContent className="grid p-0 md:grid-cols-2">
+              <form
+                className="p-6 md:p-8"
+                action={formAction}
+              >
+                <input
+                  type="hidden"
+                  name="redirect"
+                  value={redirect || ""}
+                />
+                <input
+                  type="hidden"
+                  name="priceId"
+                  value={priceId || ""}
+                />
+                <input
+                  type="hidden"
+                  name="inviteId"
+                  value={inviteId || ""}
+                />
+
+                <div className="flex flex-col gap-6">
+                  <div className="flex flex-col items-center text-center">
+                    <div className="flex justify-center mb-2">
+                      <div className="bg-primary/10 text-primary flex size-10 items-center justify-center rounded-full">
+                        <CircleIcon className="size-6 text-primary" />
+                      </div>
+                    </div>
+                    <h1 className="text-2xl font-bold">
+                      {mode === "signin" ? "Welcome back" : "Get started"}
+                    </h1>
+                    <p className="text-muted-foreground text-balance">
+                      {mode === "signin"
+                        ? "Login to your account"
+                        : "Create a new account"}
+                    </p>
+                  </div>
+
+                  <div className="grid gap-4">
+                    <div className="grid gap-2">
+                      <Label htmlFor="email">Email</Label>
+                      <Input
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="m@example.com"
+                        defaultValue={state.email}
+                        required
+                        maxLength={50}
+                        className="bg-background"
+                      />
+                    </div>
+
+                    <div className="grid gap-2">
+                      <div className="flex items-center">
+                        <Label htmlFor="password">Password</Label>
+                        {mode === "signin" && (
+                          <Link
+                            href="/forgot-password"
+                            className="ml-auto text-sm underline-offset-2 hover:underline"
+                          >
+                            Forgot your password?
+                          </Link>
+                        )}
+                      </div>
+                      <Input
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete={
+                          mode === "signin"
+                            ? "current-password"
+                            : "new-password"
+                        }
+                        defaultValue={state.password}
+                        required
+                        minLength={8}
+                        maxLength={100}
+                        className="bg-background"
+                      />
+                    </div>
+
+                    {state?.error && (
+                      <div className="text-destructive text-sm font-medium">
+                        {state.error}
+                      </div>
+                    )}
+
+                    <Button
+                      type="submit"
+                      className="w-full"
+                      disabled={pending}
+                    >
+                      {pending ? (
+                        <>
+                          <Loader2 className="animate-spin mr-2 h-4 w-4" />
+                          Loading...
+                        </>
+                      ) : mode === "signin" ? (
+                        "Login"
+                      ) : (
+                        "Sign Up"
+                      )}
+                    </Button>
+                  </div>
+
+                  <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                    <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                      or continue with
+                    </span>
+                  </div>
+
+                  <div className="grid grid-cols-3 gap-4">
+                    <Button
+                      variant="outline"
+                      className="w-full"
+                      type="button"
+                    >
+                      <svg
+                        className="h-5 w-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"></path>
+                      </svg>
+                      <span className="sr-only">Login with Apple</span>
+                    </Button>
+                    <Button
+                      variant="outline"
+                      className="w-full"
+                      type="button"
+                    >
+                      <svg
+                        className="h-5 w-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"></path>
+                      </svg>
+                      <span className="sr-only">Login with Google</span>
+                    </Button>
+                    <Button
+                      variant="outline"
+                      className="w-full"
+                      type="button"
+                    >
+                      <svg
+                        className="h-5 w-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M6.915 4.03c-1.968 0-3.683 1.28-4.871 3.113C.704 9.208 0 11.883 0 14.449c0 .706.07 1.369.21 1.973a6.624 6.624 0 0 0 .265.86 5.297 5.297 0 0 0 .371.761c.696 1.159 1.818 1.927 3.593 1.927 1.497 0 2.633-.671 3.965-2.444.76-1.012 1.144-1.626 2.663-4.32l.756-1.339.186-.325c.061.1.121.196.183.3l2.152 3.595c.724 1.21 1.665 2.556 2.47 3.314 1.046.987 1.992 1.22 3.06 1.22 1.075 0 1.876-.355 2.455-.843a3.743 3.743 0 0 0 .81-.973c.542-.939.861-2.127.861-3.745 0-2.72-.681-5.357-2.084-7.45-1.282-1.912-2.957-2.93-4.716-2.93-1.047 0-2.088.467-3.053 1.308-.652.57-1.257 1.29-1.82 2.05-.69-.875-1.335-1.547-1.958-2.056-1.182-.966-2.315-1.303-3.454-1.303zm10.16 2.053c1.147 0 2.188.758 2.992 1.999 1.132 1.748 1.647 4.195 1.647 6.4 0 1.548-.368 2.9-1.839 2.9-.58 0-1.027-.23-1.664-1.004-.496-.601-1.343-1.878-2.832-4.358l-.617-1.028a44.908 44.908 0 0 0-1.255-1.98c.07-.109.141-.224.211-.327 1.12-1.667 2.118-2.602 3.358-2.602zm-10.201.553c1.265 0 2.058.791 2.675 1.446.307.327.737.871 1.234 1.579l-1.02 1.566c-.757 1.163-1.882 3.017-2.837 4.338-1.191 1.649-1.81 1.817-2.486 1.817-.524 0-1.038-.237-1.383-.794-.263-.426-.464-1.13-.464-2.046 0-2.221.63-4.535 1.66-6.088.454-.687.964-1.226 1.533-1.533a2.264 2.264 0 0 1 1.088-.285z"></path>
+                      </svg>
+                      <span className="sr-only">Login with Meta</span>
+                    </Button>
+                  </div>
+
+                  <div className="text-center text-sm">
+                    {mode === "signin"
+                      ? "Don't have an account? "
+                      : "Already have an account? "}
+                    <Link
+                      href={`${mode === "signin" ? "/sign-up" : "/sign-in"}${
+                        redirect ? `?redirect=${redirect}` : ""
+                      }${priceId ? `&priceId=${priceId}` : ""}`}
+                      className="underline underline-offset-4"
+                    >
+                      {mode === "signin" ? "Sign up" : "Sign in"}
+                    </Link>
+                  </div>
+                </div>
+              </form>
+
+              <div className="relative hidden bg-muted md:block">
+                <Image
+                  src="https://ui.shadcn.com/placeholder.svg"
+                  alt="Image"
+                  fill
+                  className="object-cover dark:brightness-[0.95] dark:invert"
+                  priority
+                />
+              </div>
+            </CardContent>
+          </Card>
         </div>
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          {mode === "signin"
-            ? "Sign in to your account"
-            : "Create your account"}
-        </h2>
-      </div>
 
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <form
-          className="space-y-6"
-          action={formAction}
-        >
-          <input
-            type="hidden"
-            name="redirect"
-            value={redirect || ""}
-          />
-          <input
-            type="hidden"
-            name="priceId"
-            value={priceId || ""}
-          />
-          <input
-            type="hidden"
-            name="inviteId"
-            value={inviteId || ""}
-          />
-          <div>
-            <Label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </Label>
-            <div className="mt-1">
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                defaultValue={state.email}
-                required
-                maxLength={50}
-                className="appearance-none rounded-full relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-orange-500 focus:border-orange-500 focus:z-10 sm:text-sm"
-                placeholder="Enter your email"
-              />
-            </div>
-          </div>
-
-          <div>
-            <Label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </Label>
-            <div className="mt-1">
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete={
-                  mode === "signin" ? "current-password" : "new-password"
-                }
-                defaultValue={state.password}
-                required
-                minLength={8}
-                maxLength={100}
-                className="appearance-none rounded-full relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-orange-500 focus:border-orange-500 focus:z-10 sm:text-sm"
-                placeholder="Enter your password"
-              />
-            </div>
-          </div>
-
-          {state?.error && (
-            <div className="text-red-500 text-sm">{state.error}</div>
-          )}
-
-          <div>
-            <Button
-              type="submit"
-              className="w-full flex justify-center items-center py-2 px-4 border border-transparent rounded-full shadow-sm text-sm font-medium text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
-              disabled={pending}
-            >
-              {pending ? (
-                <>
-                  <Loader2 className="animate-spin mr-2 h-4 w-4" />
-                  Loading...
-                </>
-              ) : mode === "signin" ? (
-                "Sign in"
-              ) : (
-                "Sign up"
-              )}
-            </Button>
-          </div>
-        </form>
-
-        <div className="mt-6">
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <div className="w-full border-t border-gray-300" />
-            </div>
-            <div className="relative flex justify-center text-sm">
-              <span className="px-2 bg-gray-50 text-gray-500">
-                {mode === "signin"
-                  ? "New to our platform?"
-                  : "Already have an account?"}
-              </span>
-            </div>
-          </div>
-
-          <div className="mt-6">
-            <Link
-              href={`${mode === "signin" ? "/sign-up" : "/sign-in"}${
-                redirect ? `?redirect=${redirect}` : ""
-              }${priceId ? `&priceId=${priceId}` : ""}`}
-              className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-full shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
-            >
-              {mode === "signin"
-                ? "Create an account"
-                : "Sign in to existing account"}
-            </Link>
-          </div>
+        {/* 2. Legal text moved completely outside the decorated wrapper */}
+        <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
+          By clicking continue, you agree to our{" "}
+          <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
         </div>
       </div>
     </div>
