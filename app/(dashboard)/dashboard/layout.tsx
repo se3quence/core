781890658ad8504a8ded0c40de8b3279a601fe5c
thev1ndu <itsthw9@gmail.com>
2025-12-26 12:43:28 +0530

    fix: dashboard page border

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -22,11 +22,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       {/* 2. The Main Content Area */}
       <SidebarInset>
         {/* Page Content */}
-        <div className="p-2">
-          <Card className="flex flex-1 flex-col py-0 gap-0 border rounded-none">
+        <div className="flex flex-col h-full p-2">
+          <Card className="flex flex-1 flex-col py-0 gap-0 overflow-hidden">
             {/* Site Header */}
             <SiteHeader />
-            <div className="flex flex-1 flex-col gap-4 p-4 md:p-6">
+            <div className="flex flex-1 flex-col gap-4 p-4 md:p-6 overflow-auto">
               {children}
             </div>
           </Card>
